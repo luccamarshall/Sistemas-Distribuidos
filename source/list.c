70a1e19be88e5051002121f198806eae555eb3fb
rockTog <blab55153@gmail.com>
2023-09-26 15:03:42 +0100

    beca da list

--- source/list.c
+++ source/list.c
@@ -1,3 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+#include "list.h"
+#include "list-private.h"
+
+struct list_t *list_create() {
+  struct list_t *lista = (struct list_t *) malloc(sizeof(struct list_t));
+
+  if (lista == NULL)
+    return NULL;
+  
+  lista->size = 0;
+  lista->head = NULL;
+
+  return lista;
+}
