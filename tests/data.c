ffd7032db6d438c0350863aa20acaf65fae41209
rockTog <blab55153@gmail.com>
2023-10-01 15:27:12 +0100

    testes do data
    primeiros 2 testes legit

---
+++ tests/data.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include "data.h"
+
+struct data_t *data_create(int size, void *data){
+    struct data_t *data_t = (struct data_t*) malloc(sizeof(struct data_t));
+    if(data_t == NULL || size <= 0 || data == NULL) {
+        return NULL;
+    }
+    data_t->datasize = size;
+    data_t->data = data;
+    return data_t;
+}
+
+int data_destroy(struct data_t *data){
+    if(data == NULL){
+        return -1;
+    }
+    free(data->data);
+    free(data);
+    return 0;
+}
+
+struct data_t *data_dup(struct data_t *data){
+    if(data == NULL || data->datasize <= 0) {
+        return NULL;
+    }
+    struct data_t *data_t = (struct data_t*) malloc(sizeof(struct data_t));
+    if(data_t == NULL){
+        return NULL;
+    }
+    data_t->datasize = data->datasize;
+    data_t->data = malloc(data->datasize);
+    if(data_t->data == NULL) {
+        free(data_t);
+        return NULL;
+    }
+    memcpy(data_t->data, data->data, data->datasize);
+    return data_t;
+}
+
+int data_replace(struct data_t *data, int new_size, void *new_data) {
+    if(data == NULL || new_size <= 0 || new_data == NULL){
+        return -1;
+    }
+    free(data->data);
+    data->datasize = new_size;
+    data->data = new_data;
+    return 0;
+}
